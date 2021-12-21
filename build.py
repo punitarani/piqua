bf6da9249975605810ba12aa9ed4032856b1a650
Punit Sai Arani <punitsai36@gmail.com>
2021-12-20 23:30:13 -0700

    Add TDA API Authenticate option

--- build.py
+++ build.py
@@ -4,9 +4,24 @@ from configurator.create_files_folders import createFiles
 from configurator.create_files_folders import createFolders
 from configurator.generate_configJson import create_configJson
 
+from lib.tda.auth import Authenticate
+
 
 if __name__ == "__main__":
-    createFolders()
-    createFiles()
+    print("Options: ")
+    print("(1) Complete"
+          "(2) Create/Edit config.json"
+          "(3) Authenticate TDA API")
+    option = input("Enter build config option (1): ")
 
-    create_configJson(overwrite=True, take_input=True)
+    if option == 1:
+        createFolders()
+        createFiles()
+
+        create_configJson(overwrite=True, take_input=True)
+
+    if option == 2:
+        create_configJson(overwrite=True, take_input=True)
+
+    if option == 3:
+        Authenticate(generate_new_refresh_token=True)
