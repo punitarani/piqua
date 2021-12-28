b9f7feef5a223c5525b9748fe9714b719b41d33d
Punit Sai Arani <punitsai36@gmail.com>
2021-12-28 13:08:42 -0700

    Add StreamClient

--- lib/tda/__init__.py
+++ lib/tda/__init__.py
@@ -4,3 +4,4 @@ from .account import Account, Watchlist
 from .auth import get_token, get_content, authenticate
 from .equity import Equity, PriceHistory
 from .options import Option, OptionChain
+from .streamclient import StreamClient
